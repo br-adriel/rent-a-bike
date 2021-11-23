46cb8ddb22a08bd63b410c6d078fccdb882912d4
Adriel <adriel.fsantos@outlook.com>
2021-11-23 16:36:05 -0300

    Atualiza funcao limparBuffer #6

--- main.c
+++ main.c
@@ -103,7 +103,7 @@ void telaSobre(void) {
         printf("//////////////////////////////////////////////////\n");
         printf("RENT A BIKE - Sobre\n");
         printf("--------------------------------------------------\n\n");
-        printf("Versão: 0.5.6\n");
+        printf("Versão: 0.5.7\n");
         printf("Última atualização: 23/11/2021\n");
         printf("Desenvolvido por Adriel Faria dos Santos\n");
         printf("\n--------------------------------------------------\n");
--- utils.c
+++ utils.c
@@ -3,10 +3,10 @@
 
 
 void limparBuffer() {
-    // código extraído de 
-    // cprogressivo.net/2012/12/Buffer--o-que-e-como-limpar-e-as-funcoes-fflush-e-fpurge.html
-    //__fpurge(stdin); // linux
-    // fflush(stdin); // windows
+    // fonte:
+    //https://stackoverflow.com/questions/7898215/how-to-clear-input-buffer-in-c
+    char c;
+    while ( (c = getchar()) != '\n' && c != EOF ) { }
 }
 
 void msgInvalido(void) {
