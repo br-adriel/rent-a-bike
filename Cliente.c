76136ac0e720886fe4a7df7c6ebac875b55c4bd8
Adriel <adriel.fsantos@outlook.com>
2021-12-05 19:54:52 -0300

    Adiciona funcao clienteExiste #7

--- Cliente.c
+++ Cliente.c
@@ -18,3 +18,28 @@ Cliente novoCliente(char nome[], char sobrenome[], char email[])
   strcpy(cliente.email, email);
   return cliente;
 }
+
+int clienteExiste(char email[])
+{
+  char linha[150] = "";
+  int existe = 0;
+
+  FILE *arquivo;
+
+  // Cria o arquivo caso seja a primeira execução
+  arquivo = fopen("./clientes.txt", "a");
+  fclose(arquivo);
+
+  // le clientes do arquivo
+  arquivo = fopen("./clientes.txt", "r");
+  while (fgets(linha, 150, arquivo) != NULL && existe == 0)
+  {
+    // verifica se o email corresponde
+    if (strstr(linha, email))
+    {
+      existe = 1;
+    }
+  }
+  fclose(arquivo);
+  return existe;
+}
--- main.c
+++ main.c
@@ -112,7 +112,7 @@ void telaSobre(void)
         printf("//////////////////////////////////////////////////\n");
         printf("RENT A BIKE - Sobre\n");
         printf("--------------------------------------------------\n\n");
-        printf("Versão: 0.6.3\n");
+        printf("Versão: 0.6.4\n");
         printf("Última atualização: 05/12/2021\n");
         printf("Desenvolvido por Adriel Faria dos Santos\n");
         printf("\n--------------------------------------------------\n");
