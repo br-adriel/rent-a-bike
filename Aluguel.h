bb227a68a5e78e1fa2bb7566bc80693b6cb70342
Adriel <adriel.fsantos@outlook.com>
2021-12-21 17:54:09 -0300

    Adiciona assinatura de novoAluguel

--- Aluguel.h
+++ Aluguel.h
@@ -5,8 +5,10 @@ struct Aluguel
   char cliente[71];
   char bicicleta[7];
   float valor;
-  struct tm* saida;
-  struct tm* retorno;
+  struct tm *saida;
+  struct tm *retorno;
 };
 
-typedef struct Aluguel Aluguel
+typedef struct Aluguel Aluguel;
+
+Aluguel *novoAluguel(char[], char[]);
